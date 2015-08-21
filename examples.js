d7236515d90339dc5271aeaf0b8be848d6146b86
JScearcy <jacobscearcy@gmail.com>
2015-08-21 10:23:36 -0500

    Handlebars

--- examples.js
+++ examples.js
@@ -1,15 +1,25 @@
 
 $(document).ready(function() {
     var $targetDiv = $('#data');
-
-    $.ajax('data.json', {
-        success: function(response){
-            var source = $("#entry-template").html();
-            var template = Handlebars.compile(source);
-            $targetDiv.html(template(response))
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
+    function appendToDOM(){
+      $.ajax('data.json', {
+          success: function(response){
+              var source = $("#entry-template").html();
+              var template = Handlebars.compile(source);
+              $targetDiv.html(template(response))
+          },
+          error: function(request, errorType, errorMessage){
+              alert(errorType + " " + errorMessage);
+          }
+      });
+    }
+    $.ajax('handlebars.html', {
+      success: function(response){
+      $('body').append(response);
+      appendToDOM();
+    },
+      error: function(request, errorType, errorMessage){
+        alert(errorType + " " + errorMessage);
+      }
     });
 });
