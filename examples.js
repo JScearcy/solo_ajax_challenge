86d92d6627aa4164a863fc0fad656a8f0bf4b539
JScearcy <jacobscearcy@gmail.com>
2015-08-21 10:54:02 -0500

    Change color on black div

--- examples.js
+++ examples.js
@@ -1,25 +1,33 @@
 
+
 $(document).ready(function() {
-    var $targetDiv = $('#data');
-    function appendToDOM(){
-      $.ajax('data.json', {
-          success: function(response){
-              var source = $("#entry-template").html();
-              var template = Handlebars.compile(source);
-              $targetDiv.html(template(response))
-          },
-          error: function(request, errorType, errorMessage){
-              alert(errorType + " " + errorMessage);
-          }
-      });
+  Handlebars.registerHelper('textColor', function(color){
+    if(color == '#000'){
+      return 'color:#FFF;'
     }
-    $.ajax('handlebars.html', {
-      success: function(response){
-      $('body').append(response);
-      appendToDOM();
-    },
-      error: function(request, errorType, errorMessage){
-        alert(errorType + " " + errorMessage);
-      }
+  });
+  var $targetDiv = $('#Data');
+  //this function pulls the JSON file and appends the color name to dom as well as adding it as a background-color
+  function pullAndAppendToDOM(){
+    $.ajax('data.json', {
+        success: function(response){
+            var source = $("#entry-template").html();
+            var template = Handlebars.compile(source);
+            $targetDiv.html(template(response))
+        },
+        error: function(request, errorType, errorMessage){
+            alert(errorType + " " + errorMessage);
+        }
     });
+  }
+  //this function pulls the html file for the template and appends it to the index
+  $.ajax('handlebars.html', {
+    success: function(response){
+    $('body').append(response);
+    pullAndAppendToDOM();
+  },
+    error: function(request, errorType, errorMessage){
+      alert(errorType + " " + errorMessage);
+    }
+  });
 });
